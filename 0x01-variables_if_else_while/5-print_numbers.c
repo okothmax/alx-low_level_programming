b5346a2231c1e97a62a61f7b7992f935d032fea9
okothmax <okothmaxwell03@gmail.com>
2022-10-14 07:56:01 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description: prints the integers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i)
+	return (0);
+}
