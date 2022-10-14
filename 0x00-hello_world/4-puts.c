5902d8b8a907b8872977b3b21365a099aefca10b
okothmax <okothmaxwell03@gmail.com>
2022-10-14 04:33:31 -0700

    hello ;puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- *
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is building a multilingual puzzle");
 	return (0);
