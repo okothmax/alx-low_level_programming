12dcf9d9bc9287eb149d740f58b1784446675393
okothmax <okothmaxwell03@gmail.com>
2022-10-14 07:20:33 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * Desription: prints the alphabet in lower and upper case
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	char A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a)
+	}
+	printf("\n");
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A)
+	}
+	return (0);
+}
