34a7e97a6617a29c9322ef89a95aebbfc1a2a03d
okothmax <okothmaxwell03@gmail.com>
2022-10-14 06:49:21 -0700

    printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+/**
+ * main -Entry Point
+ * Description: prints the alphabet
+ * Return: 0
+ */
+int main (void)
+{ 
+	char a;
+	
+	for (a='a'; a<='z';a++)
+	{
+		putchar(a);
+	}
+	return (0);
+} 
+
+
