f319bbae6a9f63e77934ea9d0778ec842f694b11
okothmax <okothmaxwell03@gmail.com>
2022-10-17 06:54:33 -0700

    NUMBERz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - entry point
  * description: prints all numbers to base 10
@@ -10,7 +9,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		putchar((i % 10) + '0');
+	}
 	printf("\n");
 	return (0);
 }
