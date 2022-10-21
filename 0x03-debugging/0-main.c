2916a508ac9a92a88ae8bb981e98fb051cad7f2c
okothmax <okothmaxwell03@gmail.com>
2022-10-21 10:52:22 -0700

    mulriple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - testsfunction that prints if integer is positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
