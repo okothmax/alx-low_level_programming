92881e279d6a0d7d461851e78e04d3765c776788
okothmax <okothmaxwell03@gmail.com>
2022-11-09 10:13:44 -0800

    betty style fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char __attribute__((__unused__)) * argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
